756566ae4d98b5f72f4e72a0b8ae518025823bed
Chul-Woong Yang <cwyang@gmail.com>
2011-03-27 09:46:23 +0900

    buf fix

--- xterm.el
+++ xterm.el
@@ -1,22 +1,23 @@
 (load "~/emacs/xterm-frobs")
-(load "~/emacs/xterm-title")
-(setq hostname (let ((hostname (downcase (system-name))))
-		 (save-match-data
-		   (substring hostname (string-match "^[^.]+" hostname) (match-end 0)))))
-
-;; To use this, put something like the following in your .emacs:
 (when (and (not window-system)
 	   (string-match "^xterm" (getenv "TERM")))
   (require 'xterm-title)
   (xterm-title-mode 1))
 
+(setq hostname (let ((hostname (downcase (system-name))))
+		 (save-match-data
+		   (substring hostname (string-match "^[^.]+" hostname) (match-end 0)))))
+
+;; To use this, put something like the following in your .emacs:
+
 (defun my-format-mode-line (dummy &rest dummy2)
-  (let ((foo (replace-regexp-in-string "/home/cwyang" "~" buffer-file-name)))
+  (let ((foo (if (null buffer-file-name) 
+		 ""
+	       (replace-regexp-in-string "/home/cwyang" "~" buffer-file-name))))
     (concat
      (if buffer-read-only " % "
        t (if (buffer-modified-p) " * "))
      (concat "Emacs@" hostname ": ")
 					;	    (buffer-name)
-     foo
-     )))
+     foo)))
 
