2e26e96bb53a23dcebaf6e26a77ebcec6409be8e
Chul-Woong Yang <cwyang@gmail.com>
2011-08-10 00:16:27 +0900

    erlang pc setting

--- erlang.el
+++ erlang.el
@@ -1,9 +1,3 @@
-(setq load-path
-      (cons "/usr/lib/erlang/lib/tools-2.6.6.1/emacs" load-path))
-(setq erlang-root-dir "/usr/lib/erlang")
-(setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
 (setq auto-mode-alist
       (append auto-mode-alist
 	      '(("\\.[he]rl$"  . erlang-mode))))
-
-(require 'erlang-start)
