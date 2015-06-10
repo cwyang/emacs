13bf9eb2de0f0618a8fcc417682d0896fce07c2b
Chul-Woong Yang <cwyang@gmail.com>
2015-06-10 15:40:06 +0900

    ..

--- erlang.el
+++ erlang.el
@@ -1,7 +1,8 @@
 (setq load-path
-      (cons "/usr/lib/erlang/lib/tools-2.6.6.1/emacs" load-path))
-(setq erlang-root-dir "/usr/lib/erlang")
-(setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
+      (cons erlang-emacs-dir load-path))
+(setq exec-path (cons 
+		 (concat erlang-root-dir "bin")
+		 exec-path))
 (setq auto-mode-alist
       (append auto-mode-alist
 	      '(("\\.[he]rl$"  . erlang-mode))))
