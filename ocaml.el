cbfb484a0081d87c86d4d6addd74f7a441eeae43
Chul-Woong Yang <cwyang@gmail.com>
2018-02-18 09:44:14 +0900

    my-haskell.el cleaning

--- ocaml.el
+++ ocaml.el
@@ -1,12 +1,19 @@
 ; ocaml
 (let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
-  (when (and opam-share (file-directory-p opam-share))
-    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
-    (autoload 'merlin-mode "merlin" nil t nil)
-    (add-hook 'tuareg-mode-hook 'merlin-mode t)
-      (add-hook 'caml-mode-hook 'merlin-mode t)))
+      (when (and opam-share (file-directory-p opam-share))
+       ;; Register Merlin
+       (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+       (autoload 'merlin-mode "merlin" nil t nil)
+       ;; Automatically start it in OCaml buffers
+       (add-hook 'tuareg-mode-hook 'merlin-mode t)
+       (add-hook 'caml-mode-hook 'merlin-mode t)
+       ;; Use opam switch to lookup ocamlmerlin binary
+       (setq merlin-command 'opam)))
+;tuareg
 (add-to-list 'load-path "/home/cwyang/.opam/4.06.0/share/emacs/site-lisp")
-(require 'ocp-indent)
+;company
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'merlin-company-backend))
 (add-hook 'merlin-mode-hook 'company-mode)
+;ocp-indent
+(require 'ocp-indent)
