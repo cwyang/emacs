5059d48b42168f3ae0d24ee1bf856ff779f29d2b
Chul-Woong Yang <cwyang@gmail.com>
2020-03-24 11:18:49 +0900

    go.el

--- go.el
+++ go.el
@@ -1,3 +1,42 @@
+; gopls / lsp-mode
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+;; company-lsp integrates company mode completion with lsp-mode.
+;; completion-at-point also works out of the box but doesn't support snippets.
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+;; Optional - provides snippet support.
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
+
 (defun my-go-mode-hook ()
   (setq tab-width 4 indent-tabs-mode 1)
   (go-eldoc-setup)
@@ -11,3 +50,4 @@
     (define-key map (kbd "C-c .") 'go-test-current-test)
     (define-key map (kbd "C-c b") 'go-run)))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+(add-hook 'go-mode-hook #'lsp)
