ae62e672b42e3b64b7d570d895ea2425cf28ebdb
Chul-Woong Yang <cwyang@gmail.com>
2020-03-24 11:28:59 +0900

    clear non-working python setting

--- my-python.el
+++ my-python.el
@@ -1,5 +1,8 @@
-(elpy-enable)
-(setq elpy-rpc-python-command "python3"
-      python-indent-guess-indent-offset t
-      python-indent-guess-indent-offset-verbose nil)
+;; (use-package elpy
+;;   :ensure t
+;;   :init
+;;   (elpy-enable))
+;; (setq elpy-rpc-python-command "python3"
+;;       python-indent-guess-indent-offset t
+;;       python-indent-guess-indent-offset-verbose nil)
 
