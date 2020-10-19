51982722b17db9feb59a0d52467650ac6ecd25a2
Y <ylinux2@gmail.com>
2020-10-19 18:12:41 +0900

    rust racer-mode

--- my-rust.el
+++ my-rust.el
@@ -4,3 +4,16 @@
   :init
   (with-eval-after-load 'rust-mode
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
+;; rustup toolchain add nightly
+;; rustup component add rust-src
+;; cargo +nightly install racer
