35db68b884cf4f2a77d2584878d6fe1af7a35853
Chul-Woong Yang <cwyang@gmail.com>
2020-10-25 22:39:14 +0900

    MELPA paredit

---
+++ my-parenthesis.el
@@ -0,0 +1,18 @@
+;; parenthesis
+(load "match-paren")
+(global-set-key "%" 'match-paren)
+
+(use-package paredit
+  :ensure t
+  :config
+  (autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+  (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+  (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1))))
+
+(use-package mic-paren
+  :ensure t
+  :config
+  (paren-activate) ; activating
+  (setq paren-priority 'close))
