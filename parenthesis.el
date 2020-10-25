93a17d6e029ed8bdc158c52cc8eaba1f06030af8
Chul-Woong Yang <cwyang@gmail.com>
2020-10-25 22:28:21 +0900

    mic-paren

--- parenthesis.el
+++ parenthesis.el
@@ -8,6 +8,8 @@
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 
+(paren-activate) ; activating
+(setf paren-priority 'close)
 ;(when (or (string-match "XEmacs\\|Lucid" emacs-version) window-system)
 ;  (require 'mic-paren) ; loading
 ;  (paren-activate) ; activating
