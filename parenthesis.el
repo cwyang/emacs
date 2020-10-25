3c7968f613fc19a41b3864c96b5fed49bfcf15fa
Chul-Woong Yang <cwyang@gmail.com>
2020-10-25 22:20:17 +0900

    temporarily disable mic-paren
    Since my Macbook Emacs cannot load it.

--- parenthesis.el
+++ parenthesis.el
@@ -8,9 +8,9 @@
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 
-(when (or (string-match "XEmacs\\|Lucid" emacs-version) window-system)
-  (require 'mic-paren) ; loading
-  (paren-activate) ; activating
-  (setf paren-priority 'close))
+;(when (or (string-match "XEmacs\\|Lucid" emacs-version) window-system)
+;  (require 'mic-paren) ; loading
+;  (paren-activate) ; activating
+;  (setf paren-priority 'close))
 
 (global-set-key "%" 'match-paren)
